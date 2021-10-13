5301cf8f73417260bb7c583011f35eb75856c66a
Philip <philipmjobe@gmail.com>
2021-10-13 18:53:54 -0400

    added fifth and sixth actor

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,6 @@
 #Actor.create(first_name: "Kevin", last_name: "Smith", known_for: "Clerks")
 #Actor.create(first_name: "Tobin", last_name: "Bell", known_for: "Saw")
 #Actor.create(first_name: "Tony", last_name: "Todd", known_for: "Candyman")
-Actor.create(first_name: "Bill", last_name: "Moseley", known_for: "House of 1000 Corpses")
+#Actor.create(first_name: "Bill", last_name: "Moseley", known_for: "House of 1000 Corpses")
+Actor.create(first_name: "Kane", last_name: "Hodder", known_for: "Friday the 13th Part VII: The New Blood")
+Actor.create(first_name: "Doug", last_name: "Bradley", known_for: "Hellraiser")
