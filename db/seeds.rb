b5a03f578843969f2a2842e7f1a2551c6cb59c3b
Philip <philipmjobe@gmail.com>
2021-10-13 18:50:04 -0400

    added fourth actor

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@
 #Actor.create(first_name: "Kevin", last_name: "Smith", known_for: "Clerks")
 #Actor.create(first_name: "Tobin", last_name: "Bell", known_for: "Saw")
 #Actor.create(first_name: "Tony", last_name: "Todd", known_for: "Candyman")
+Actor.create(first_name: "Bill", last_name: "Moseley", known_for: "House of 1000 Corpses")
