29e822981a2b3bf5ed1243e584a642ae314d146c
Philip <philipmjobe@gmail.com>
2021-10-14 17:29:09 -0400

    added what i think i messed up yesterday

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,5 +4,10 @@ class ActorsController < ApplicationController
     person = Actor.find_by id: 1
     render json: person.as_json
   end
-  
+
+  def all_actors
+    person = Actor.all
+    render json: person.as_json
+  end 
+
 end
