40d882b44d55e5708f72d6360aa294b5c5c9a22f
Philip <philipmjobe@gmail.com>
2021-10-14 18:44:29 -0400

    added movies to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 #Actor.create({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 #Actor.create({first_name: "Bill", last_name: "Moseley", known_for: "House of 1000 Corpses"})
 #Actor.create({first_name: "Kane", last_name: "Hodder", known_for: "Friday the 13th Part VII: The New Blood,"})
+
+# Movie.create({title: "Halloween", year: 1978, plot: "The plot tells about a mental patient who was committed to a sanitarium for murdering his babysitting teenage sister on Halloween night when he was six years old. Fifteen years later, he escapes and returns to his hometown, where he stalks a female babysitter and her friends, while under pursuit by his psychiatrist."})
+# Movie.create({title: "Train to Busan", year: 2016, plot: "The film mostly takes place on a high-speed train from Seoul to Busan as a zombie apocalypse suddenly breaks out in the country and threatens the safety of the passengers."})
+# Movie.create({title: "Phantasm", year: 1979, plot: "The first film in the Phantasm franchise, it introduces the Tall Man (Angus Scrimm), a supernatural and malevolent undertaker who turns the dead of Earth into dwarf zombies to be sent to his planet and used as slaves. He is opposed by a young boy, Mike (Michael Baldwin), who tries to convince his older brother Jody (Bill Thornbury) and family friend Reggie (Reggie Bannister) of the threat."})
