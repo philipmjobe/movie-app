71da9c89fdf3340ab119812d7b8645849bea865d
Philip <philipmjobe@gmail.com>
2021-10-14 08:51:25 -0400

    added proper indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get "/single_actor" => "actors#single_actor"
+  get "/single_actor" => "actor#single_actor"
 end
