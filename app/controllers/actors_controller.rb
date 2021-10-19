c0f8a99e69b7d6b537783d4b3437b78950ee80de
Philip <philipmjobe@gmail.com>
2021-10-19 09:20:33 -0400

    updated actors controllers to restful practices

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,19 +1,37 @@
 class ActorsController < ApplicationController
 
-  def actors 
-    input = params["id"]
-    person = Actor.find_by id: input
-    render json: person.as_json
-  end
-
-  def all_actors
-    person = Actor.all
-    render json: person.as_json
+  def index
+    output = Actor.all  
+    render json: output.as_json
   end 
 
-  def single_actors
-    actor = Actor.find_by[:id]
-    render json: {message: actor.as_json}
+  def create
+    actor = Actor.new(first_name: params["first_name"], last_name: params["last_name"], known_for: params["known_for"])
+    actor.save
+    render json: actor.as_json
+  end 
+
+  def show
+    input = params["id"]
+    output = Actor.find_by id: input
+    render json: output.as_json
+  end 
+
+  def update
+    input = params["id"]
+    actor = Actor.find_by id: input
+    actor.first_name = params["first_name"] || actor.first_name
+    actors.last_name = params["last_name"] || actor.last_name
+    actor.known_for = params["known_for"] || actors.known_for
+    actor.save 
+    render json: actor.as_json
+  end 
+
+  def destroy
+    input = params["id"].to_i
+    actor = Actor.find_by id: input
+    actor.destroy 
+    render json: {message: "Actor Destroyed"}
   end 
 
 end
