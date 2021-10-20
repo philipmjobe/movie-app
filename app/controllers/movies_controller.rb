ec160fee7c400649f49e5e828ae48993d9da3b48
Philip <philipmjobe@gmail.com>
2021-10-20 18:05:43 -0400

    added a movie with updated migrations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   end 
 
   def create
-    movie = Movie.new(title: params["title"], year: params["year"], plot: params["plot"])
+    movie = Movie.new(title: params["title"], year: params["year"], plot: params["plot"], director: params["director"], english: params["english"])
     movie.save
     render json: movie.as_json
   end 
