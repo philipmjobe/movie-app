1600c1880e9f34eb10acae58510c84f9449222ac
Philip <philipmjobe@gmail.com>
2021-10-20 18:21:40 -0400

    added gender and age updates to actor controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,7 +6,7 @@ class ActorsController < ApplicationController
   end 
 
   def create
-    actor = Actor.new(first_name: params["first_name"], last_name: params["last_name"], known_for: params["known_for"])
+    actor = Actor.new(first_name: params["first_name"], last_name: params["last_name"], known_for: params["known_for"], gender: params["gender"], age: params["age"])
     actor.save
     render json: actor.as_json
   end 
@@ -21,8 +21,10 @@ class ActorsController < ApplicationController
     input = params["id"]
     actor = Actor.find_by id: input
     actor.first_name = params["first_name"] || actor.first_name
-    actors.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actors.known_for
+    actor.last_name = params["last_name"] || actor.last_name
+    actor.known_for = params["known_for"] || actor.known_for
+    actor.gender = params["gender"] || actor.gender
+    actor.age = params["age"] || actor.age
     actor.save 
     render json: actor.as_json
   end 
