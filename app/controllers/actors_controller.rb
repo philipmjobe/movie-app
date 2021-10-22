d652e00cf9a8734d0abbcbb240cff10a6e8843a1
Philip <philipmjobe@gmail.com>
2021-10-22 09:19:04 -0400

    added happy/sad paths to actors_controller.rb

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,8 +7,12 @@ class ActorsController < ApplicationController
 
   def create
     actor = Actor.new(first_name: params["first_name"], last_name: params["last_name"], known_for: params["known_for"], gender: params["gender"], age: params["age"])
-    actor.save
-    render json: actor.as_json
+    
+    if actor.save 
+      render json: actor.as_json
+    else  
+      render json: {error: actor.errors.full_messages}, status: 406
+    end 
   end 
 
   def show
@@ -25,8 +29,12 @@ class ActorsController < ApplicationController
     actor.known_for = params["known_for"] || actor.known_for
     actor.gender = params["gender"] || actor.gender
     actor.age = params["age"] || actor.age
-    actor.save 
-    render json: actor.as_json
+    
+    if actor.save 
+      render json: actor.as_json
+    else  
+      render json: {error: actor.errors.full_messages}, status: 406
+    end 
   end 
 
   def destroy
