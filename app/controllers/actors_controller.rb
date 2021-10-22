856ba54e5d1ae82efa0d67c01fabea1ae389faf7
Philip <philipmjobe@gmail.com>
2021-10-22 18:11:33 -0400

    made updates to controllers and some of the movies/actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,7 +6,7 @@ class ActorsController < ApplicationController
   end 
 
   def create
-    actor = Actor.new(first_name: params["first_name"], last_name: params["last_name"], known_for: params["known_for"], gender: params["gender"], age: params["age"])
+    actor = Actor.new(first_name: params["first_name"], last_name: params["last_name"], known_for: params["known_for"], gender: params["gender"], age: params["age"], movie_id: params["movie_id"])
     
     if actor.save 
       render json: actor.as_json
@@ -29,6 +29,7 @@ class ActorsController < ApplicationController
     actor.known_for = params["known_for"] || actor.known_for
     actor.gender = params["gender"] || actor.gender
     actor.age = params["age"] || actor.age
+    actor.movie_id = params["movie_id"] || actor.movie_id
     
     if actor.save 
       render json: actor.as_json
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :actors 
   validates :title, presence: true
   validates :year, presence: true, numericality: { only_integer: true}
   validates :plot, presence: true, length: {minimum: 10}
