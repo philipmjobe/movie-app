5377ba9fb9a3c1e7e192c3b40fc9d82f1f6457d8
Philip <philipmjobe@gmail.com>
2021-10-22 09:26:14 -0400

    added validations in movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, presence: true, numericality: { only_integer: true}
+  validates :plot, presence: true, length: {minimum: 10}
 end
