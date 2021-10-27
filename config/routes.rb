b1426c9cac9b276416d0de0c005813e8d2ebc8c5
Philip <philipmjobe@gmail.com>
2021-10-27 18:04:23 -0400

    added post route to routes and created users conrtoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+### ACTORS RESTFUL ACTIONS 
   get "/actors" => "actors#index"
   post "/actors" => "actors#create"
   get "/actors/:id" => "actors#show"
   patch "/actors/:id" => "actors#update"
   delete "/actors/:id" => "actors#destroy"
   
+### MOVIES RESTFUL ACTIONS 
   get "/movies" => "movies#index"
   post "/movies" => "movies#create"
   get "/movies/:id" => "movies#show"
   patch "/movies/:id" => "movies#update"
   delete "/movies/:id" => "movies#destroy"
+
+### USERS RESTFUL ACTIONS
+  post "/users" => "users#create"
 end
