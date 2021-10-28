2f7d89bc8293308ddbd0d264f13f2317fa49d6b0
Philip <philipmjobe@gmail.com>
2021-10-28 18:33:20 -0400

    added genre.rb to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_222812) do
+ActiveRecord::Schema.define(version: 2021_10_28_223224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2021_10_27_222812) do
     t.string "gender"
     t.integer "age"
     t.integer "movie_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "movies", force: :cascade do |t|
