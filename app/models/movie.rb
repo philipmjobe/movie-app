10338b3b33db6ecc088dcabbfa46acc7377bcc75
Philip <philipmjobe@gmail.com>
2021-10-28 18:54:06 -0400

    added serializer gem

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors 
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   validates :title, presence: true
   validates :year, presence: true, numericality: { only_integer: true}
   validates :plot, presence: true, length: {minimum: 10}
