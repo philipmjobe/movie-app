bc446c57b15f29a2388d868842d97a348dfb4011
Philip <philipmjobe@gmail.com>
2021-11-04 11:57:11 -0400

    added the rest of the stuff to the database and did a seed dump to get my stuff back in the seeds.rb file

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,5 @@
 class Actor < ApplicationRecord
-  belongs_to :movie 
+  belongs_to :movie, optional: true
   validates :first_name, length: {minimum: 2}
   validates :last_name, length: {minimum: 2}
   validates :known_for, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+# Genre.create!([
+#   {name: "Horror"},
+#   {name: "Comedy"}
+# ])
+# Actor.create!([
+#   {first_name: "Tony", last_name: "Todd", known_for: "Candyman", gender: "male", age: 66, movie_id: nil},
+#   {first_name: "Doug", last_name: "Bradley", known_for: "Hellraiser", gender: "male", age: 67, movie_id: nil},
+#   {first_name: "David Howard", last_name: "Thorton", known_for: "Terrifer", gender: "male", age: 42, movie_id: nil},
+#   {first_name: "Bill", last_name: "Moseley", known_for: "House of 1000 Corpses", gender: "male", age: 69, movie_id: nil},
+#   {first_name: "Kane", last_name: "Hodder", known_for: "Friday the 13th Part VII: The New Blood,", gender: "male", age: 66, movie_id: nil},
+#   {first_name: "Kevin", last_name: "Smith", known_for: "Clerks", gender: "male", age: 51, movie_id: nil}
+# ])
 # Movie.create!([
 #   {title: "Clerks", year: 1994, plot: "it presents a day in the lives of titular store clerks Dante Hicks (O'Halloran) and Randal Graves (Anderson), along with their acquaintances.", director: nil, english: true},
 #   {title: "Candyman", year: 1992, plot: "the film follows a Chicago graduate student completing a thesis on urban legends and folklore, which leads her to the legend of the Candyman, the ghost of an artist and the son of a slave who was murdered in the late 19th century for his relationship with the daughter of a wealthy white man.", director: nil, english: true},
@@ -6,21 +18,14 @@
 #   {title: "House of 1000 Corpses", year: 2003, plot: "The plot centers on a group of teenagers who are kidnapped and tortured by a psychotic family, during Halloween, after traveling across the country to write a book.", director: nil, english: true},
 #   {title: "Friday the 13th Part VII: The New Blood", year: 1988, plot: "Set years after the events of Friday the 13th Part VI: Jason Lives, the plot follows a psychokinetic teenage girl (Lincoln) who unwittingly releases Jason from his tomb at the bottom of Crystal Lake, allowing him to go on another killing spree in the area.", director: nil, english: true}
 # ])
-Actor.create!([
-  {first_name: "Tony", last_name: "Todd", known_for: "Candyman", gender: "male", age: 66, movie_id: nil},
-  {first_name: "Doug", last_name: "Bradley", known_for: "Hellraiser", gender: "male", age: 67, movie_id: nil},
-  {first_name: "David Howard", last_name: "Thorton", known_for: "Terrifer", gender: "male", age: 42, movie_id: nil},
-  {first_name: "Bill", last_name: "Moseley", known_for: "House of 1000 Corpses", gender: "male", age: 69, movie_id: nil},
-  {first_name: "Kane", last_name: "Hodder", known_for: "Friday the 13th Part VII: The New Blood,", gender: "male", age: 66, movie_id: nil},
-  {first_name: "Kevin", last_name: "Smith", known_for: "Clerks", gender: "male", age: 51, movie_id: nil}
-])
-
-# Genre.create(name: "Horror")
-# Genre.create(name: "Comedy")
-
-# MovieGenre.create(movie_id: 6, genre_id: 2)
-# MovieGenre.create(movie_id: 7, genre_id: 1)
-# MovieGenre.create(movie_id: 8, genre_id: 1)
-# MovieGenre.create(movie_id: 9, genre_id: 1)
-# MovieGenre.create(movie_id: 10, genre_id: 1)
-# MovieGenre.create(movie_id: 11, genre_id: 1)
+# MovieGenre.create!([
+#   {genre_id: 2, movie_id: 1},
+#   {genre_id: 1, movie_id: 2},
+#   {genre_id: 1, movie_id: 3},
+#   {genre_id: 1, movie_id: 4},
+#   {genre_id: 1, movie_id: 5},
+#   {genre_id: 1, movie_id: 6}
+# ])
+# User.create!([
+#   {name: "Philip", email: "phil@example.com", password_digest: "$2a$12$G/xON./Z999ib9TG.dTMsuRWspDM4XeI.caOuzJqUoKN4v2x6b9pC", admin: true}
+# ])
